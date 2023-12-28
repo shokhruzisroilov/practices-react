193610fcc553b430a1b46b0290385b13b477f1ab
shokhruzisroilov <shokhruzisroilov@gmail.com>
2023-12-28 10:40:03 +0500

    localstorage is done

--- use-fetch/src/App.jsx
+++ use-fetch/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import User from './components/User'
 import Loader from './components/Loader'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useFetch } from './costumHooks/useFetch'
 
+const local = JSON.parse(localStorage.getItem('userCount'))
+
 const App = () => {
-	const [count, setCount] = useState(1)
+	const [count, setCount] = useState(local)
 	const { users, loading } = useFetch(count === 0 ? 1 : count)
+
+	useEffect(() => {
+		localStorage.setItem('userCount', JSON.stringify(count))
+	}, [count])
 	return (
 		<div className='app'>
 			<button
